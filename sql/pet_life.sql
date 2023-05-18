d7d34cafe5dc28a6f8bf4b8905ca213456a9974a
lianyabing <lianyabing@santiyun.com>
2023-05-18 23:33:54 +0800

    sql

--- sql/pet_life.sql
+++ sql/pet_life.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost_5.7
  Source Server Type    : MySQL
- Source Server Version : 50734
+ Source Server Version : 50734 (5.7.34-log)
  Source Host           : localhost:3306
  Source Schema         : pet_life
 
  Target Server Type    : MySQL
- Target Server Version : 50734
+ Target Server Version : 50734 (5.7.34-log)
  File Encoding         : 65001
 
- Date: 02/05/2022 13:35:52
+ Date: 18/05/2023 23:30:09
 */
 
 SET NAMES utf8mb4;
@@ -43,7 +43,15 @@ CREATE TABLE `adopt_apply`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '申请领养表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '申请领养表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of adopt_apply
+-- ----------------------------
+INSERT INTO `adopt_apply` VALUES (1, 1, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', '小明', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', 23, 1, '1', '1', '2', '金融', '杭州', '13100200001', '1232131', '想养狗', '0', '0', '2023-05-18 21:40:38', '2023-05-18 23:20:37');
+INSERT INTO `adopt_apply` VALUES (2, 1, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', '哈哈[呲牙]', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', 25, 2, '1', '1', '2', '金融', '杭州', '13198567084', 'ghjk', '哈哈[呲牙]', '0', '0', '2023-05-18 21:57:49', '2023-05-18 23:20:37');
+INSERT INTO `adopt_apply` VALUES (3, 1, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp01', '小白', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', 25, 2, '1', '1', '2', '金融', '杭州', '13100000085', 'hjja', '哈哈哈', '0', '0', '2023-05-18 22:50:51', '2023-05-18 23:20:37');
+INSERT INTO `adopt_apply` VALUES (5, 1, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', '小白', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', 25, 0, '1', '1', '2', '互联网', '杭州', '13100000001', 'hjjbvfh', '测试一下', '1', '0', '2023-05-18 23:20:11', '2023-05-18 23:20:37');
 
 -- ----------------------------
 -- Table structure for answer_prize
@@ -60,6 +68,12 @@ CREATE TABLE `answer_prize`  (
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '答题表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of answer_prize
+-- ----------------------------
+INSERT INTO `answer_prize` VALUES (1, '猜一猜：北京冬奥会火炬“雪花”，如何保证燃料充足持续燃烧', '上方威亚吊着气路,把气瓶藏在花瓣里', '上方威亚吊着气路', '我们和地球', '0', '2022-02-17 14:58:13', '2022-02-25 16:41:10');
+INSERT INTO `answer_prize` VALUES (2, '这是一个题目', '选项一,选项二', '选项一', '测试类别', '0', '2022-04-05 11:30:30', '2022-04-05 11:30:29');
 
 -- ----------------------------
 -- Table structure for article
@@ -80,6 +94,15 @@ CREATE TABLE `article`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '文章表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of article
+-- ----------------------------
+INSERT INTO `article` VALUES (1, '沙雕集锦', '狗子沙雕集锦', 'https://mp.weixin.qq.com/s/bNjidkegbuBVrtxGl7uOZA', '#FCF5C9', 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.zhimg.com%2Fv2-6df7121d5e4955182c835d77f826c773_r.jpg%3Fsource%3D1940ef5c&refer=http%3A%2F%2Fpic2.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646472663&t=1c7f3796198783c41685d8e4262d0046', 0, '0', '2022-02-03 16:09:21', '2023-03-02 21:23:31');
+INSERT INTO `article` VALUES (2, '养宠百科', '狗子沙雕集锦', 'https://mp.weixin.qq.com/s/bNjidkegbuBVrtxGl7uOZA', '#EEF9FF', 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.zhimg.com%2Fv2-6df7121d5e4955182c835d77f826c773_r.jpg%3Fsource%3D1940ef5c&refer=http%3A%2F%2Fpic2.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646472663&t=1c7f3796198783c41685d8e4262d0046', 0, '0', '2022-02-03 16:09:21', '2022-02-03 17:48:31');
+INSERT INTO `article` VALUES (3, '萌宠故事', '狗子沙雕集锦', 'https://mp.weixin.qq.com/s/bNjidkegbuBVrtxGl7uOZA', '#F2FCE8', 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.zhimg.com%2Fv2-6df7121d5e4955182c835d77f826c773_r.jpg%3Fsource%3D1940ef5c&refer=http%3A%2F%2Fpic2.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646472663&t=1c7f3796198783c41685d8e4262d0046', 0, '0', '2022-02-03 16:09:21', '2022-02-03 17:48:29');
+INSERT INTO `article` VALUES (4, '领表情包', '狗子沙雕集锦', 'https://mp.weixin.qq.com/s/bNjidkegbuBVrtxGl7uOZA', '#FFEEEF', 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.zhimg.com%2Fv2-6df7121d5e4955182c835d77f826c773_r.jpg%3Fsource%3D1940ef5c&refer=http%3A%2F%2Fpic2.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646472663&t=1c7f3796198783c41685d8e4262d0046', 0, '0', '2022-02-03 16:09:21', '2022-02-03 17:48:28');
+INSERT INTO `article` VALUES (5, '领表情包', '狗子沙雕集锦', 'https://mp.weixin.qq.com/s/bNjidkegbuBVrtxGl7uOZA', '#F2FCE8', 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.zhimg.com%2Fv2-6df7121d5e4955182c835d77f826c773_r.jpg%3Fsource%3D1940ef5c&refer=http%3A%2F%2Fpic2.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646472663&t=1c7f3796198783c41685d8e4262d0046', 0, '0', '2022-02-03 16:09:21', '2022-02-03 17:48:27');
+
+-- ----------------------------
 -- Table structure for chat
 -- ----------------------------
 DROP TABLE IF EXISTS `chat`;
@@ -93,7 +116,14 @@ CREATE TABLE `chat`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '聊天表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '聊天表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of chat
+-- ----------------------------
+INSERT INTO `chat` VALUES (15, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '你好', '1', '0', '2022-09-28 16:53:44', '2022-09-28 17:44:03');
+INSERT INTO `chat` VALUES (16, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', '有事请留言', '1', '0', '2022-09-28 17:44:20', '2022-09-28 17:44:24');
+INSERT INTO `chat` VALUES (17, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '你好 我想领养你的狗狗', '1', '0', '2023-05-18 21:45:58', '2023-05-18 21:46:08');
 
 -- ----------------------------
 -- Table structure for collect
@@ -107,7 +137,11 @@ CREATE TABLE `collect`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '收藏表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '收藏表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of collect
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for collect_user_answer
@@ -119,6 +153,13 @@ CREATE TABLE `collect_user_answer`  (
   `type` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '答题结果 1-答对 2-答错',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户答题关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of collect_user_answer
+-- ----------------------------
+INSERT INTO `collect_user_answer` VALUES ('oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', '2', '2022-02-17 18:14:03');
+INSERT INTO `collect_user_answer` VALUES ('oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', '1', '2022-02-18 10:21:39');
+INSERT INTO `collect_user_answer` VALUES ('oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '2', '1', '2022-04-05 11:30:57');
 
 -- ----------------------------
 -- Table structure for comment
@@ -137,7 +178,20 @@ CREATE TABLE `comment`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '评论表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '评论表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (16, NULL, NULL, '11', 'CIRCLE', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '1', '2022-02-03 01:27:41', '2022-02-15 13:54:48');
+INSERT INTO `comment` VALUES (17, NULL, NULL, '111', 'CIRCLE', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '1', '2022-02-12 19:24:26', '2022-02-15 13:54:50');
+INSERT INTO `comment` VALUES (18, NULL, NULL, '1111', 'CIRCLE', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '1', '2022-02-15 14:13:10', '2022-02-15 14:13:15');
+INSERT INTO `comment` VALUES (19, NULL, NULL, '2121', 'CIRCLE', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '1', '2022-02-15 14:13:52', '2022-02-15 14:13:53');
+INSERT INTO `comment` VALUES (20, NULL, NULL, '111', 'CIRCLE', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '0', '2022-02-15 14:14:03', NULL);
+INSERT INTO `comment` VALUES (21, NULL, NULL, '222', 'CIRCLE', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '0', '2022-02-15 14:14:05', NULL);
+INSERT INTO `comment` VALUES (22, NULL, NULL, '111', 'FIND', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '1', '2022-02-15 14:25:26', '2022-02-15 14:25:49');
+INSERT INTO `comment` VALUES (23, NULL, NULL, '312232131', 'FIND', 3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '0', '2022-02-15 14:25:31', NULL);
+INSERT INTO `comment` VALUES (24, NULL, NULL, '111', 'CIRCLE', 7, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 0, '0', '2022-09-27 21:02:09', NULL);
 
 -- ----------------------------
 -- Table structure for credits
@@ -153,6 +207,11 @@ CREATE TABLE `credits`  (
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '积分表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of credits
+-- ----------------------------
+INSERT INTO `credits` VALUES (1, 'oEgDe5clk5QtAgQSrDhq7ajKiVzE', 0, 8, '0', '2022-01-30 17:36:41', '2022-01-31 17:58:43');
 
 -- ----------------------------
 -- Table structure for pet_adopt
@@ -179,7 +238,12 @@ CREATE TABLE `pet_adopt`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '宠物领养' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '宠物领养' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pet_adopt
+-- ----------------------------
+INSERT INTO `pet_adopt` VALUES (1, '我的狗狗', '小黄', '13100000000', '2', 'https://pet-life.oss-cn-beijing.aliyuncs.com/', '\"images/2023/05/18/PdKQbPPdiXT3142a346cf36b7adee35f89674f6ef0a8.jpg\"', '1', '已免疫, 已驱虫, 已绝育', '4-6月', '仅限同城, 工作稳定, 接收回访, 签订领养协议, 不得遗弃、转让、贩卖、虐待', '武汉', '测试', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '0', 81, '0', '2023-05-18 21:34:53', '2023-05-18 23:20:52');
 
 -- ----------------------------
 -- Table structure for pet_circle
@@ -194,7 +258,12 @@ CREATE TABLE `pet_circle`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '动态' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '动态' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pet_circle
+-- ----------------------------
+INSERT INTO `pet_circle` VALUES (7, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '哈哈哈哈哈', '\"images/2022/09/27/M0tNZRtDBQyic40895490ce7d72b8e9995b8d11638e3.png\"', '0', '2022-09-27 20:58:28', '2022-09-27 20:58:28');
 
 -- ----------------------------
 -- Table structure for pet_cyclopedia
@@ -213,6 +282,15 @@ CREATE TABLE `pet_cyclopedia`  (
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '宠物科普' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pet_cyclopedia
+-- ----------------------------
+INSERT INTO `pet_cyclopedia` VALUES (1, '新人养宠', NULL, '这是新人养宠', 1, '1', '#FCF5C9', '0', '2022-01-30 13:59:08', '2023-03-02 21:18:59');
+INSERT INTO `pet_cyclopedia` VALUES (2, '抚养护理', NULL, '这是抚养护理', 4, '1', '#EEF9FF', '0', '2022-01-30 13:59:38', '2023-03-02 21:18:53');
+INSERT INTO `pet_cyclopedia` VALUES (3, '基础病例', NULL, '这是基础病例', 3, '1', '#FFEEEF', '0', '2022-01-30 14:00:01', '2022-02-12 18:37:23');
+INSERT INTO `pet_cyclopedia` VALUES (4, '猫咪呕吐没食欲', 'https://img1.baidu.com/it/u=931545919,4030748642&fm=253&fmt=auto&app=138&f=JPEG?w=306&h=459', '猫咪呕吐没食欲猫咪呕吐没食欲猫咪呕吐没食欲猫咪呕吐没食欲', 3, '2', NULL, '0', '2022-01-30 14:03:58', '2022-01-30 21:16:09');
+INSERT INTO `pet_cyclopedia` VALUES (5, '猫咪一直舔毛的原因', 'https://img0.baidu.com/it/u=358489060,1008822084&fm=253&fmt=auto&app=138&f=JPEG?w=654&h=500', '<div style=\"color: red;\">猫咪一直舔毛的原因</div>猫咪一直舔毛的原因猫咪一直舔毛的原因猫咪一直舔毛的原因', 211, '2', NULL, '0', '2022-01-30 14:04:36', '2023-03-02 21:18:56');
 
 -- ----------------------------
 -- Table structure for pet_find
@@ -239,7 +317,12 @@ CREATE TABLE `pet_find`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`, `is_deleted`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '寻宠' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '寻宠' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pet_find
+-- ----------------------------
+INSERT INTO `pet_find` VALUES (5, '狗狗丢失', '小王', '13222222222', '金毛', '2', 'https://pet-life.oss-cn-beijing.aliyuncs.com/', '\"images/2022/09/27/wVNwa0hqZXF4c40895490ce7d72b8e9995b8d11638e3.png\"', '1', '7-12月', '杭州', '2222', '121222', 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', '1', 5, '0', '2022-09-27 21:03:15', '2023-05-18 23:11:05');
 
 -- ----------------------------
 -- Table structure for user_follow
@@ -253,7 +336,11 @@ CREATE TABLE `user_follow`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户关注表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户关注表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_follow
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for wx_user
@@ -278,6 +365,12 @@ CREATE TABLE `wx_user`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of wx_user
+-- ----------------------------
+INSERT INTO `wx_user` VALUES (3, 'oh2iA5D5r4ICUzQOwuxz2Kqvba3Y', 'khSQZSB1WC4Aaq9uBLOgYw==', 'null', '微信用户', 'https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132', '', '', '', '0', '', NULL, 'WECHAT_MP', '0', '0', '2022-01-31 21:23:23', '2023-05-18 23:16:31');
+INSERT INTO `wx_user` VALUES (4, 'oh2iA5AVCtjvCfxA9XKPDB0yqhp0', 'jgynVPkm8Gfp0LyJI7jxtA==', 'null', '微信用户', 'https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132', '', '', '', '0', NULL, NULL, 'WECHAT_MP', '0', '0', '2023-05-18 21:39:43', '2023-05-18 23:17:59');
 
 SET FOREIGN_KEY_CHECKS = 1;
